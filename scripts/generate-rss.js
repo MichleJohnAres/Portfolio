4ae2ee86fded5c15a699bf9a2abcc4f6c39f8355
michlejohn <michlejohnares@outlook.com>
2024-07-01 09:24:41 -0600

    change blog

--- scripts/generate-rss.js
+++ scripts/generate-rss.js
@@ -16,26 +16,5 @@ exports.generate = async () => {
     copyright: `Michle John | ${new Date().getFullYear()}`,
   });
 
-  const posts = await fs.readdir(path.join(__dirname, "..", "data", "blogs"));
-
-  await Promise.all(
-    posts.map(async (filename) => {
-      const blogsData = await fs.readFile(
-        path.join(path.join(__dirname, "..", "data", "blogs", filename))
-      );
-
-      const frontmatter = matter(blogsData);
-
-      feed.item({
-        title: frontmatter.data.title,
-        description: frontmatter.data.subtitle,
-        url: frontmatter.data.url,
-        author: "Michle John",
-        date: frontmatter.data.date,
-        categories: frontmatter.data.tags.split(", "),
-      });
-    })
-  );
-
   await fs.writeFile("./public/rss.xml", feed.xml({ indent: true }));
 };
